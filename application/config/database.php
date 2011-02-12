51201ff367e2019d45c32189f14c7cec2b5ace7d
Matteo Mangoni <aoikagesan@gmail.com>
2011-02-12 14:55:05 +0100

    PHPOnlineMangaReader

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
+$db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'hkmdb';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
